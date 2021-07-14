5b7af17059481b2bc27da8d6e48749225e47fd12
Anupriya-Kashyap <anupriya.kashyap@newgenapps.com>
2021-07-14 16:01:36 +0530

    port updated

--- server/app.py
+++ server/app.py
@@ -56,4 +56,4 @@ def follow_tweet():
 if __name__ == '__main__' :
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=3000)
--- server/static/results2.js
+++ server/static/results2.js
@@ -1,2 +1,2 @@
-var follower_data={'0': 'INFP', '1': 'INFP', '2': 'ISFP', '3': 'ISFP', '4': 'ESFP'}
-var follower_ids={'0': 'AumaObama', '1': 'springsteen', '2': 'CarterCenter', '3': 'BillClinton', '4': 'KamalaHarris'}
+var follower_data={'0': 'ISFP', '1': 'INFP', '2': 'ISTJ', '3': 'ENFP', '4': 'INFP'}
+var follower_ids={'0': 'Snapdragon_IN', '1': 'MonuSood_', '2': 'FinTaxBot', '3': 'RiotSupport', '4': 'rashtrapatibhvn'}
